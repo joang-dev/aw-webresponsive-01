24bef8e7d27b58c1edb7ef83d49ffdcacd0e2490
joang-dev <joanngrau06@gmail.com>
2024-11-19 13:50:25 +0100

    Menu icon shadow and hover

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const menuIcon = document.getElementById("menuToggle")
 const body = document.querySelector("body")
 function displayMenu () {
-    if (body.hasAttribute("class")) {
-        body.removeAttribute("class")
+    if (body.hasAttribute("id")) {
+        body.removeAttribute("id")
     } else {
-        body.setAttribute("class", "noNav")
+        body.setAttribute("id", "noNav")
+    }
+
+    menuActive()
+}
+
+function menuActive () {
+    if (menuIcon.classList.contains("active")) {
+        menuIcon.classList.remove("active")
+    } else {
+        menuIcon.classList.add("active")
     }
 }
 
